9e67d9eec9f3286a0a79908907a299512c6d5148
Zouhair Laaroussi <zlaarous@e3r3p12.1337.ma>
2024-10-14 14:58:56 +0100

    customize login page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,13 @@
 import { useState } from 'react'
 import './App.css'
-
-import { Button } from '@/components/ui/button'
+import Login from './pages/Login'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div>
-      <Button>Click me</Button>
+      <Login />
     </div>
   )
 }
